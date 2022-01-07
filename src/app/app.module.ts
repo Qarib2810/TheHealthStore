e099a1affc70435b507bf27ac986ed5382013f3c
Qarib2810 <qarib2810@gmail.com>
2022-01-07 18:59:24 +0530

    Form Validation Done For Sign-in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   declarations: [AppComponent, HelloComponent, SignInComponent],
   bootstrap: [AppComponent],
 })
